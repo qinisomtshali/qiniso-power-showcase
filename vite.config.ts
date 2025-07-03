874c68eb1b24da37eeb2411543cd152609bcd504
Qiniso Sakhiwo Mtshali <qiniso@hearconnect.co.za>
2025-07-03 16:35:43 +0200

    Revert GitHub Pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/qiniso-power-showcase/',
+  
   server: {
     host: "::",
     port: 8080,
