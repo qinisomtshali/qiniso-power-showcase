1c5b103dc164add246f17c8eabdf8148b065e76d
Qiniso Sakhiwo Mtshali <qiniso@hearconnect.co.za>
2025-07-03 11:15:56 +0200

    not deploying

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  
+  // 👇 This line is the most important part for GitHub Pages!
+  base: mode === 'production' ? '/qiniso-power-showcase/' : '/',
   server: {
     host: "::",
     port: 8080,
