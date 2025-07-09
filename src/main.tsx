e13b95e06583564c27ec1f2711473b375a8bb010
Qiniso Sakhiwo Mtshali <qiniso@hearconnect.co.za>
2025-07-09 16:05:17 +0200

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Analytics } from '@vercel/analytics/next'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>
+);
